5cbcace201d8c13bd50d9662e2c3924a8541cc8d
Steve Shogren <steve.a.shogren@gmail.com>
2015-06-16 07:15:05 -0400

    Getting it to work with the memoized solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Provides a memoized value along with the data structure
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmemo <- function(memo) m <<- memo
+  getmemo <- function() m
+  list(set = set, get = get,
+       setmemo = setmemo,
+       getmemo = getmemo)
 }
 
 
-## Write a short comment describing this function
-
+## inverses a cacheMatrix, otherwise returning
+## the memoized value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmemo()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
